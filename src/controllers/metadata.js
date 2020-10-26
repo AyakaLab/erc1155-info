62672c28ad58ace3a8e1317023e9c16bde7b6bfb
Ayaka Neko <neko@ayaka.moe>
2020-10-26 19:12:30 +0800

    :sparkles: Added a new endpoint for id only query

--- src/controllers/metadata.js
+++ src/controllers/metadata.js
@@ -6,14 +6,23 @@ const Log = require('../util/log')
 
 let Metadata = {
     async getMetadata (ctx, next) {
-        let params = ctx.request.params
-        Log.debug(params)
         if (!ctx.params.id || !ctx.params.set) {
             ctx.status = 404
             ctx.body = { status: -1, message: 'missing params'}
         }
+        if (ctx.params.id.indexOf('.json')) ctx.params.id = ctx.params.id.replace('.json', '')
         ctx.type = 'application/json'
-        ctx.body = fs.createReadStream('./assets/metadata/' + ctx.params.set + '/' + ctx.params.id)
+        ctx.body = fs.createReadStream('./assets/metadata/' + ctx.params.set + '/' + ctx.params.id + '.json')
+        await next()
+    },
+    async getMetadataIdOnly (ctx, next) {
+        if (!ctx.params.id) {
+            ctx.status = 404
+            ctx.body = { status: -1, message: 'missing params'}
+        }
+        if (ctx.params.id.indexOf('.json')) ctx.params.id = ctx.params.id.replace('.json', '')
+        ctx.type = 'application/json'
+        ctx.body = fs.createReadStream('./assets/metadata/' + ctx.params.id + '.json')
         await next()
     }
 }
