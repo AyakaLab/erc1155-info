32c993e7151a25ce0b395add406acea6101b6799
Ayaka Neko <neko@ayaka.moe>
2020-10-26 19:11:41 +0800

    :art: Adjusted metadata routing pattern

--- src/api/metadata.js
+++ src/api/metadata.js
@@ -1,8 +1,9 @@
 const KoaRouter = require('koa-router')
 
 const MetaRouter = new KoaRouter()
-const { getMetadata } = require('../controllers/metadata')
+const { getMetadata, getMetadataIdOnly } = require('../controllers/metadata')
 
+MetaRouter.get('/:id', getMetadataIdOnly)
 MetaRouter.get('/:set/:id', getMetadata)
 
 module.exports = MetaRouter
